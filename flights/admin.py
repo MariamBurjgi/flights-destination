e5607c22b6fdafbd82f6a81a401e9ab62906b9d4
programming <MariamBurjgi>
2023-06-08 19:47:52 +0400

    adding django admin panel

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Flight, Airport, Passenger
 
 # Register your models here.
+admin.site.register(Flight)
+admin.site.register(Airport)
+admin.site.register(Passenger)
+
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+# Register your models here.
+admin.site.register(Flight, FlightAdmin)
